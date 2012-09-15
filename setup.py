2f5346eae20485bd2fbe5df68b38292f842b02d7
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-09-15 16:57:09 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,36 @@
-try:
-    import setuptools
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
 from setuptools import setup, find_packages
+import os
 
 version = '0.0.1'
-readme = open('README.rst').read()
+here = os.path.abspath(os.path.dirname(__file__))
+long_description = open(os.path.join(here, 'README.rst')).read()
 
 setup(name='hat',
       version=version,
-      description='Highlight for cat and git diff',
-      long_description=readme,
+      description='cat with highlighted with pygments',
+      long_description=long_description,
       classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
-          'Topic :: Software Development :: Documentation',
-          'Topic :: Software Development :: Libraries'
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
       ],
-      keywords='cat highlight git diff',
-      author='Bernardo Barreto Marques',
+      keywords='test bdd nosetests spec unit',
+      author='Bernardo B. Marques',
       author_email='bernardo.fire@gmail.com',
-      url='www.github.com/bernardofire/hat',
-      license='MIT License',
-      packages=find_packages()
+      url='http://github.com/bernardofire/hat',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'pygments'
+      ],
+      entry_points="""
+      [console_scripts]
+      hat = hat:main
+      """,
       )
