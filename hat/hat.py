fc17730ee2ebe964162277522a0fd52329be1c53
Douglas Camata <d.camata@gmail.com>
2012-09-24 11:23:10 -0300

    pep8

--- hat/hat.py
+++ hat/hat.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def cat(argv):
-  try:
-      sys.exit(os.system("pygmentize -f terminal %s" % argv[1]))
-  except KeyboardInterrupt:
-      sys.exit(1)
+    try:
+        sys.exit(os.system("pygmentize -f terminal %s" % argv[1]))
+    except KeyboardInterrupt:
+        sys.exit(1)
